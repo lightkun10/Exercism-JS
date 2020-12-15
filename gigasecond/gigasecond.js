5e9388d936115423a6bf19540b21a49601b9d163
lightkun10 <lifeisubw@gmail.com>
2020-12-15 14:35:09 +0700

    Finish gigasecond

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const GIGASECOND_IN_MS = Math.pow(10, 9) * 1000
+
+export const gigasecond = (date) => new Date(date.getTime() + GIGASECOND_IN_MS);
